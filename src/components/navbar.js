00dc6d9605f7aa1797d70a4ff97fee8a76e82e88
Caleb Dunn <finish.06@gmail.com>
2022-01-22 20:56:52 -0600

    Remove graphs links

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -17,8 +17,7 @@ class MyNavbar extends React.Component {
                     <Nav>
                         <Nav.Link href="/#about">About</Nav.Link>
                         <Nav.Link href="/#resume">Resume</Nav.Link>
-                        <Nav.Link href="/#certs">Certificates</Nav.Link>
-                        <Nav.Link href="/graphs">Home Temps</Nav.Link>
+                        {/* <Nav.Link href="/graphs">Home Temps</Nav.Link> */}
                     </Nav>
                 </Navbar>
             </Container>
